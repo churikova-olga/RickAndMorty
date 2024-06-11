582de81a52e1be93725bc4e9128cd19fd0cf7499
churikova-olga <olga_churikova890@mail.ru>
2024-06-11 18:48:03 +0200

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/RickAndMorty/'
-})
-
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV === "production" ? "/RickAndMorty/" : "/"
+// })
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+};
