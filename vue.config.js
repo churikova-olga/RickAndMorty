37e8a5dd30c1604364d8a2f9277c485e038a76cb
churikova-olga <olga_churikova890@mail.ru>
2024-06-11 18:52:37 +0200

    update config

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@
 //   publicPath: process.env.NODE_ENV === "production" ? "/RickAndMorty/" : "/"
 // })
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/RickAndMorty/" : "/",
 };
